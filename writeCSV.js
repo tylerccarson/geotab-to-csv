c25ed8340dcc81055c93099ffe508df1fea473c8
tyler carson <tyler.c.carson@gmail.com>
2018-02-09 00:05:49 -0800

    mvp

---
+++ writeCSV.js
@@ -0,0 +1,37 @@
+const axios = require('axios');
+const json2csv = require('json2csv');
+const fs = require('fs');
+const credentials = require('./credentials.js');
+
+//issue request for data
+axios.get('https://my198.geotab.com/apiv1/Get', {
+  params: {
+    typeName: 'Device',
+    credentials: {
+      database: credentials.database,
+      userName: credentials.userName,
+      password: credentials.password
+    }
+  }
+})
+  .then((res) => {
+    //manipulate JSON to access just the results w/ map function, possibly for the fields wanted (need to add date, some other things)
+    var results = res.data.result;
+
+    //parse JSON into a csv file with json2csv
+    const csv = json2csv({data: results});
+
+    //write as csv
+    fs.writeFile('geotabresults.csv', csv, (res, err) => {
+      if (err) throw Error;
+    });
+
+    //write as JSON, then use the CLI to convert to .csv
+    // fs.writeFile('geotabresults.json', results, (res, err) => {
+    //   if (err) throw Error;
+    // });
+
+  })
+  .catch((err) => {
+    console.log(err);
+  });
